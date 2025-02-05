d31a1584868be6b62fb64e87f52310ced775c80d
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-02-04 22:13:57 -0500

    feat(components): add Education component and update ResumeForm to use setData
    - Added `Education` component to `ResumeForm`
    - Updated `ResumeForm` to pass `setData` for managing state updates
    - Ensured consistency across form components by using `setData`

--- src/components/ResumeForm.jsx
+++ src/components/ResumeForm.jsx
@@ -1,11 +1,15 @@
 import PersonalForm from './PersonalForm';
 import Summary from './SummaryForm';
+import EmploymentForm from './EmploymentForm';
+import Education from './Education';
 
-function ResumeForm({ data, handleChange }) {
+function ResumeForm({ data, handleChange, setData }) {
   return (
     <div className="resume-form">
       <PersonalForm data={data} handleChange={handleChange} />
       <Summary data={data} handleChange={handleChange} />
+      <EmploymentForm data={data} setData={setData} />
+      <Education data={data} setData={setData} />
     </div>
   );
 }
